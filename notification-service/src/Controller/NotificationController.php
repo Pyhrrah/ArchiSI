f6eac5a076291b12e3e9b6e9110dc3f36d2a7332
anafernandes181004@gmail.com <anafernandes181004@gmail.com>
2024-07-13 18:13:09 +0200

    modifs notification

--- notification-service/src/Controller/NotificationController.php
+++ notification-service/src/Controller/NotificationController.php
@@ -20,35 +20,27 @@ class NotificationController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    /**
-     * @Route("/api/notification/send-email", name="send_email_notification", methods={"POST"})
-     */
     public function sendEmailNotification(Request $request, MailerInterface $mailer)
     {
-        // Récupérer les données du corps de la requête JSON
         $data = json_decode($request->getContent(), true);
 
-        // Valider et récupérer les détails de l'email
         $emailRecipient = $data['emailRecipient'] ?? null;
         $subject = $data['subject'] ?? 'Notification';
         $message = $data['message'] ?? 'This is a notification message.';
-        $type = $data['type'] ?? 'email'; // Vous pouvez définir une valeur par défaut ou gérer différemment
+        $type = $data['type'] ?? 'email';
 
-        // Vérifier si l'email du destinataire est présent
         if (!$emailRecipient) {
             return new JsonResponse(['error' => 'Email recipient is required.'], 400);
         }
 
-        // Envoyer l'email
         $email = (new Email())
-            ->from('your_email@example.com') // Remplacer par votre adresse email
+            ->from('your_email@example.com') 
             ->to($emailRecipient)
             ->subject($subject)
             ->text($message);
 
         $mailer->send($email);
 
-        // Enregistrer les détails de l'email dans la base de données
         $notification = new Notification();
         $notification->setType($type);
         $notification->setEmailRecipient($emailRecipient);
@@ -59,7 +51,6 @@ class NotificationController extends AbstractController
         $this->entityManager->persist($notification);
         $this->entityManager->flush();
 
-        // Réponse JSON en cas de succès
         return new JsonResponse(['message' => 'Email sent and notification saved.']);
     }
 }

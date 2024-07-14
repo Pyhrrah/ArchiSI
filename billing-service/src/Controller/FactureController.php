87f9a20a7eca011854fbaa48cf79e25c2332ba47
Elivyde <mathurin.elias@gmail.com>
2024-07-14 11:35:00 +0200

    envoi du billing modifié

--- billing-service/src/Controller/FactureController.php
+++ billing-service/src/Controller/FactureController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Facture;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FactureController extends AbstractController
 {
